a030df372f026e05cca71cf90485594841aca8fe
solov <soloviov.alexandr2012@yandex.ru>
2023-02-14 19:20:49 +0300

    Упражнение

--- src/main/java/vasilev/input/InputDialogDemo.java
+++ src/main/java/vasilev/input/InputDialogDemo.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class InputDialogDemo {
     public static void main(String[] args) {
         String text;
-        text = JOptionPane.showInputDialog("Введите текст");
-        JOptionPane.showMessageDialog(null, "Вы ввели такой текст:\n" + text);
+        text = JOptionPane.showInputDialog("Р’РІРµРґРёС‚Рµ С‚РµРєСЃС‚");
+        JOptionPane.showMessageDialog(null, "Р’С‹ РІРІРµР»Рё С‚Р°РєРѕР№ С‚РµРєСЃС‚:\n" + text);
     }
 }
