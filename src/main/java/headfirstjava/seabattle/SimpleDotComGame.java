36bc85376b7874199013678eadd0b1381649aa0f
solov <soloviov.alexandr2012@yandex.ru>
2023-02-21 22:21:35 +0300

    Морской бой

---
+++ src/main/java/headfirstjava/seabattle/SimpleDotComGame.java
@@ -0,0 +1,22 @@
+package headfirstjava.seabattle;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] location = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(location);
+        boolean isAlive = true;
+        while (isAlive = true) {
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkUserSelf(guess);
+            numOfGuesses++;
+            if (result.equals("Потопил")) {
+                isAlive = false;
+                System.out.println("Вам потребовалось" + numOfGuesses + "попыток(и)");
+            }
+        }
+    }
+}
---
+++ src/tests/java/headfirstjava/seabattle/SimpleDotComTestDrive.java
@@ -0,0 +1,16 @@
+package headfirstjava.seabattle;
+
+class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        SimpleDotCom dot = new SimpleDotCom();
+        int[] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+        String userGuess = "2";
+        String result = dot.checkUserSelf(userGuess);
+        String testResult = "Неудача";
+        if (result.equals("Попал")) {
+            testResult = "Пройден";
+        }
+        System.out.println(testResult);
+    }
+}
